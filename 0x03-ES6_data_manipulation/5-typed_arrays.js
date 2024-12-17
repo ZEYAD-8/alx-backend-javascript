0cfbb9a340302cd993f3c0cac1b6add4c067828b
ZEYAD-8 <zeyadosama15@gmail.com>
2024-12-17 02:35:20 +0200

    Lint Compliance

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-	const buffer = new ArrayBuffer(length);
+  const buffer = new ArrayBuffer(length);
 
-	const view = new Int8Array(buffer);
-	if (position > view.length) {
-		throw new Error('Position outside ranges');
-	}
-	view[position] = value;
+  const view = new Int8Array(buffer);
+  if (position > view.length) {
+    throw new Error('Position outside ranges');
+  }
+  view[position] = value;
 
-	return new DataView(buffer);
+  return new DataView(buffer);
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,20 @@
 export default function cleanSet(set, startString) {
-	let str = '';
+  let str = '';
 
-	if (startString === '') {
-		return '';
-	}
-	for (const item of set) {
-		if (set instanceof Set && typeof startString === 'string') {
-			if (item && item.startsWith(startString)) {
-				const strSlice = item.slice(startString.length);
-				if (str !== '') {
-					str += '-';
-				}
-				str += strSlice;
-			}
-		}
-	}
+  if (startString === '') {
+    return '';
+  }
+  for (const item of set) {
+    if (set instanceof Set && typeof startString === 'string') {
+      if (item && item.startsWith(startString)) {
+        const strSlice = item.slice(startString.length);
+        if (str !== '') {
+          str += '-';
+        }
+        str += strSlice;
+      }
+    }
+  }
 
-	return str;
+  return str;
 }
