e22ffed40af8a7b72625fda62e4aba06d356d258
ZEYAD-8 <zeyadosama15@gmail.com>
2024-12-18 03:03:31 +0200

    Updated

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,8 +1,20 @@
-import Car from './10-car';
+import Car from './car.js';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    this._range = range;
+  }
+
+  static get [Symbol.clone]() {
+    return Car;
   }
 }
