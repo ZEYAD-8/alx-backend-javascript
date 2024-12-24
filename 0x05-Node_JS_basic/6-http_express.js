423a151caa2fd03d66ad7aadda3d055c48bc93b7
ZEYAD-8 <zeyadosama15@gmail.com>
2024-12-24 05:06:32 +0200

    Using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,20 +1,14 @@
 const express = require('express');
 
 const app = express();
+const PORT = 1245;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server running');
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
-// What is the main difference between the two snippets?
-
-// The second snippet uses the Express framework to create a server. 
-// Express is a web application framework for Node.js, 
-// designed for building web applications and APIs.
-// It is more flexible and provides more features 
-// than the built-in http module.
